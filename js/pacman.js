52c3ae8778c27f410ea56ab4b022a884fd078f83
Youssef Makhoul <Youssef@Joes-MB-Pro.local>
2018-10-16 12:35:03 -0400

    mobile friendly

--- js/pacman.js
+++ js/pacman.js
@@ -72,7 +72,79 @@ function getRandomDirection() {
     return 'up';
 }
 
+class Swipe {
+    constructor(element) {
+        this.xDown = null;
+        this.yDown = null;
+        this.element = typeof (element) === 'string' ? document.querySelector(element) : element;
 
+        this.element.addEventListener('touchstart', function (evt) {
+            this.xDown = evt.touches[0].clientX;
+            this.yDown = evt.touches[0].clientY;
+        }.bind(this), false);
+
+    }
+
+    onLeft(callback) {
+        this.onLeft = callback;
+
+        return this;
+    }
+
+    onRight(callback) {
+        this.onRight = callback;
+
+        return this;
+    }
+
+    onUp(callback) {
+        this.onUp = callback;
+
+        return this;
+    }
+
+    onDown(callback) {
+        this.onDown = callback;
+
+        return this;
+    }
+
+    handleTouchMove(evt) {
+        if (!this.xDown || !this.yDown) {
+            return;
+        }
+
+        var xUp = evt.touches[0].clientX;
+        var yUp = evt.touches[0].clientY;
+
+        this.xDiff = this.xDown - xUp;
+        this.yDiff = this.yDown - yUp;
+
+        if (Math.abs(this.xDiff) > Math.abs(this.yDiff)) { // Most significant.
+            if (this.xDiff > 0) {
+                this.onLeft();
+            } else {
+                this.onRight();
+            }
+        } else {
+            if (this.yDiff > 0) {
+                this.onUp();
+            } else {
+                this.onDown();
+            }
+        }
+
+        // Reset values.
+        this.xDown = null;
+        this.yDown = null;
+    }
+
+    run() {
+        this.element.addEventListener('touchmove', function (evt) {
+            this.handleTouchMove(evt);
+        }.bind(this), false);
+    }
+}
 
 // This section is where you will be doing most of your coding
 class Entity {
@@ -312,21 +384,48 @@ class Engine {
         this.restartButton = new RestartButton();
         this.score = 0;
         this.lastFrame = Date.now();
+        let movePlayerLeft = () => {
+            this.player.move(MOVE_LEFT);
+        };
+        movePlayerLeft = movePlayerLeft.bind(this);
+
+        let movePlayerRight = () => {
+            this.player.move(MOVE_RIGHT);
+        };
+        movePlayerRight = movePlayerRight.bind(this);
+
+        let movePlayerUp = () => {
+            this.player.move(MOVE_UP);
+        };
+
+        movePlayerUp = movePlayerUp.bind(this);
+
+        let movePlayerDown = () => {
+            this.player.move(MOVE_DOWN);
+        };
+        movePlayerDown = movePlayerDown.bind(this);
+
+        var swiper = new Swipe(document);
+        swiper.onLeft(movePlayerLeft);
+        swiper.onRight(movePlayerRight);
+        swiper.onUp(movePlayerUp);
+        swiper.onDown(movePlayerDown);
         let keydownHandler = function (e) {
             if (e.keyCode === LEFT_ARROW_CODE) {
-                this.player.move(MOVE_LEFT);
+                movePlayerLeft();
             } else if (e.keyCode === RIGHT_ARROW_CODE) {
-                this.player.move(MOVE_RIGHT);
+                movePlayerRight();
             } else if (e.keyCode === UP_ARROW_CODE) {
-                this.player.move(MOVE_UP);
+                movePlayerUp();
             } else if (e.keyCode === DOWN_ARROW_CODE) {
-                this.player.move(MOVE_DOWN);
+                movePlayerDown();
             } else if (e.keyCode === 32)
                 location.reload();
         };
         keydownHandler = keydownHandler.bind(this);
         // Listen for keyboard left/right and update the player
         document.addEventListener('keydown', keydownHandler);
+        swiper.run();
         sounds.background.play();
         this.gameLoop();
     }
